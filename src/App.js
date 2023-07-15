78c203bb8bb2c0d59a18f1f553564cccdd3061a2
arnavsharma <arnavsh2004@gmail.com>
2023-07-15 13:08:47 +0530

    lifting state up to parent component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
 import React from "react";
 import { useState } from "react";
 
-function Square() {
-  const [value,setValue] = useState(null);
-
-  function handleClick() {
-    setValue('X')
-  }
+function Square({ value, onSquareClick }) {
 
   return (
-    <button onClick={handleClick} className="square">
+    <button className="square" onClick={onSquareClick} >
       {value}
     </button>
   );
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <React.Fragment>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} onSquareClick={handleClick} />
+        <Square value={squares[2]} onSquareClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={handleClick} />
+        <Square value={squares[4]} onSquareClick={handleClick} />
+        <Square value={squares[5]} onSquareClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={handleClick} />
+        <Square value={squares[7]} onSquareClick={handleClick} />
+        <Square value={squares[8]} onSquareClick={handleClick} />
       </div>
     </React.Fragment>
   );
